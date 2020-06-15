c0fe2566ac39d267c317a55ac1ac0e26865b352b
Oleksandr Kononenko <s_com@ukr.net>
2020-06-15 15:13:40 +0300

    var types

--- sandbox/src/main/java/ua/innerbox/FirstProg.java
+++ sandbox/src/main/java/ua/innerbox/FirstProg.java
@@ -1,8 +1,12 @@
 package ua.innerbox;
 
 public class FirstProg {
-	
-	public static void main(String[] args) {
-	System.out.println("Hello, world!");
-	}
+
+    public static void main(String[] args) {
+        String something = "world";
+        System.out.println("Hello, " + something + "!");
+        double l = 8;
+        double s = l*l;
+        System.out.println("Площадь квадрата со стороно " + l + " = " + s);
+    }
 }
