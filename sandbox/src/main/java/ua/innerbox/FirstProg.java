49d7905433ab34f98d48ca398506f78f5a25543f
Oleksandr Kononenko <s_com@ukr.net>
2020-06-15 19:18:44 +0300

    function work, simple examples

--- sandbox/src/main/java/ua/innerbox/FirstProg.java
+++ sandbox/src/main/java/ua/innerbox/FirstProg.java
@@ -3,10 +3,27 @@ package ua.innerbox;
 public class FirstProg {
 
     public static void main(String[] args) {
-        String something = "world";
+        hello("world");
+        hello("user");
+        hello("Oleksandr");
+        double b = 3;
+        System.out.println("Площадь квадрата, co стороной " + b+ " равна: " + area(b) );
+
+        double a1 = 4;
+        double a2 = 6;
+        System.out.println("Площадь прямоугольника, co стороной " + a1 + " и " + a2 + " равна: " + area(a1,a2));
+    }
+
+    public static void hello(String something) {
+//        String something = "world";
         System.out.println("Hello, " + something + "!");
-        double l = 8;
-        double s = l*l;
-        System.out.println("Площадь квадрата со стороно " + l + " = " + s);
+    }
+
+    public static double area(double b) {
+        return b*b;
+    }
+
+    public static double area(double a, double b) {
+        return a*b;
     }
 }
