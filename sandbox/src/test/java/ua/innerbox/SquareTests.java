7ceaa1df3a0fba825a46826f958b7afe3fd50f59
Oleksandr Kononenko <s_com@ukr.net>
2020-06-18 22:20:46 +0300

    square test added, test lib imported

---
+++ sandbox/src/test/java/ua/innerbox/SquareTests.java
@@ -0,0 +1,13 @@
+package ua.innerbox;
+
+
+import org.testng.annotations.Test;
+
+public class SquareTests {
+    @Test
+    public void testArea() {
+        Square s = new Square(5);
+        assert s.area() == 25;
+
+    }
+}
